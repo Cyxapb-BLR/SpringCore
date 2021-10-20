b197b970bac0129688a2bc7588315e8c7d39b392
Cyxapb-BLR <cyxapb-511@yandex.ru>
2021-10-20 14:12:42 -0400

    6_create loggers.xml, modified spring.xml( import from loggers.xml)

--- src/main/java/com/yet/spring/core/App.java
+++ src/main/java/com/yet/spring/core/App.java
@@ -3,7 +3,6 @@ package com.yet.spring.core;
 import com.yet.spring.core.beans.Client;
 import com.yet.spring.core.beans.Event;
 import com.yet.spring.core.loggers.EventLogger;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
